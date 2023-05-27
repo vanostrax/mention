6b42d8ea09180db182bb1c0dea9b1a4f5844c3f3
Vanostra <109914394+vanostrax@users.noreply.github.com>
2023-05-27 11:00:53 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,7 +99,7 @@ async def mentionall(event):
     if not chat_id in spam_chats:
       break
     usrnum += 1
-    usrtxt += f"✆ [{usr.first_name}](tg://user?id={usr.id}) "
+    usrtxt += f"✆ [{usr.first_name}](tg://user?id={usr.id})\n"
     if usrnum == 5:
       if mode == "text_on_cmd":
         txt = f"{usrtxt}\n\n{msg}"
